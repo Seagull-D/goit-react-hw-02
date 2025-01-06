23dd0428fc80f5a294a64a3c0d6e484e5b3f3dbf
Dmytro Chaika <djseagull90@gmail.com>
2025-01-06 17:23:58 +0100

    seting project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,6 @@
 
 import './App.css'
-import Profile from './Profile/Profile'
-import userData from '../userData.json';
-import FriendList from './FriendList/FriendList';
-import friends from "../friends.json";
-import TransactionHistory from './TransactionHistory/TransactionHistory';
-import transactions from "../transactions.json";
+
 const App = () => {
   return (
     <>
